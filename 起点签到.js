2517907e0d5c1a517b41a98d6239493281fdbb5f
luqin2007 <lq2007lq@hotmail.com>
2024-02-20 16:23:45 +0800

    game

--- 起点签到.js
+++ 起点签到.js
@@ -1,6 +1,9 @@
 // 初始化
 console.show();
 auto.waitFor();
+
+const regex_ad = /观看视频(\d+)秒后，可获得奖励/
+const regex_game = /(\d+)\/(\d+)分钟/
 let textView;
 
 // 启动起点
@@ -50,50 +53,57 @@ while(textView = findView("看视频")) {
 log("限时彩蛋 结束");
 // #endregion
 
+// 领奖励
+log("领奖励 开始");
+while(textView = findView("领奖励")) {
+    clickButton(textView);
+    sleep(1000);
+    if (textView = findView("我知道了")) {
+        clickButton(textView);
+    }
+}
+log("领奖励 结束");
 
-// 玩游戏
-// log("玩游戏 开始");
-// if ((textView = findView("当日玩游戏\\d+分钟", "match"))) {
-//     let layout = textView.parent();
-//     textView = layout.findOne(text("去完成"));
-//     if (textView) {
-//         // 计算剩余时间
-//         let text = layout.findOne(textMatches("\\d+/\\d+分钟"));
-//         if (text) {
-//             text = text.text();
-//             let result = text.match("(\\d+)/(\\d+)分钟");
-//             let gameTimes = parseInt(result[1]);
-//             let allTime = parseInt(result[2]);
-//             let playMinutes = Math.max(allTime - gameTimes, 1);
-//             // 玩游戏
-//             clickButton(findView("去完成"));
-//             waitForActivity("com.qidian.QDReader.ui.activity.QDBrowserActivity");
-//             clickButton(waitView("新游"));
-//             waitForActivity("com.qidian.QDReader.ui.activity.GameBrowserActivity");
-//             clickButton(waitView("在线玩"));
-//             sleep(1000);
-//             log("保持屏幕常亮");
-//             device.keepScreenDim();
-//             for (let i = playMinutes + 1; i > 0; --i) {
-//                 log(`剩余 ${i}min`);
-//                 sleep(66000);
-//                 device.wakeUpIfNeeded();
-//             }
-//             log("停止屏幕常亮，游戏挂机结束");
-//             device.cancelKeepingAwake();
-//             device.vibrate(500);
-//             // 游戏页(无标题) - 新游 - 游戏中心 - 福利中心
-//             while (true) {
-//                 if (findView("福利中心")) {
-//                     break;
-//                 }
-//                 back();
-//                 sleep(1000);
-//             }
-//         }
-//     }
-// }
-// log("玩游戏 结束");
+// #region 玩游戏
+log("玩游戏 开始");
+if (textView = findView("当日玩游戏10分钟")) {
+    let layout = textView.parent();
+    let playView = layout.findOne(text("去完成"));
+    if (playView) {
+        // 计算剩余时间
+        textView = layout.findOne(text("/10分钟")).parent();
+        views = textView.find(className("TextView"));
+        views.forEach(o => log(o));
+        textView = views[views.length - 2];
+
+        let gameTimes = parseInt(textView.text());
+        log(textView.text());
+        let playMinutes = Math.max(10 - gameTimes, 1);
+        log(playMinutes);
+        // 玩游戏
+        clickButton(playView);
+        waitForActivity("com.qidian.QDReader.ui.activity.QDBrowserActivity");
+        clickButton(waitView("新游"));
+        waitForActivity("com.qidian.QDReader.ui.activity.GameBrowserActivity");
+        clickButton(waitView("在线玩"));
+        sleep(1000);
+        for (let i = playMinutes + 1; i > 0; --i) {
+            log(`剩余 ${i}min`);
+            for (let j = 0; j < 60; ++j) {
+                sleep(1100);
+                device.wakeUpIfNeeded();
+            }
+        }
+        device.vibrate(500);
+        // 游戏页(无标题) - 新游 - 游戏中心 - 福利中心
+        while (!findView("福利中心")) {
+            back();
+            sleep(1000);
+        }
+    }
+}
+log("玩游戏 结束");
+// #endregion
 
 // 领奖励
 log("领奖励 开始");
@@ -116,7 +126,18 @@ log("运行结束");
 sleep(5000);
 console.hide();
 
-// region 
+/**
+ * 根据正则表达式查找字符串中的值
+ * @param {string} str 字符串
+ * @param {RegExp} regex 正则表达式
+ * @param {number|undefined} count 结果个数
+ * @returns 当数量不少于需要的个数时，返回以 1 开头的数组
+ */
+function findValueFromString(str, regex, count) {
+    if (!count) count = 1;
+    let m = regex.exec(str);
+    return (m && m.length >= count + 1) ? m : undefined;
+}
 
 /**
  * 查找带有某个文本的控件
@@ -186,24 +207,18 @@ function clickButton(view) {
  * @returns 是否播放完成
  */
 function watchAds() {
-    const regex = /观看视频(\d+)秒后，可获得奖励/
-
     className("ImageView").waitFor();
     id("android:id/navigationBarBackground").waitFor();
     sleep(5000);
     if (textView = findView("观看视频\\d+秒后，可获得奖励", 'match')) {
-        let adTime = 45; // 应该不会有比 45s 更长的广告了吧
-        let m = regex.exec(textView.text());
-        if (m && m.length > 1) {
-            adTime = m[1];
-        }
+        let adTime = findValueFromString(textView.text(), regex_ad);
+        // 应该不会有比 45s 更长的广告了吧
+        adTime = adTime ? adTime[1] : 45;
         log(`广告时间：${adTime}+1s`);
         sleep(adTime * 1000);
         sleep(1000); // 额外休眠 1s
     } else if (textView = findView("观看完视频，可获得奖励")) {
-        while (findView("跳过广告")) {
-            sleep(1000);
-        }
+        while (findView("跳过广告")) sleep(1000);
     } else if (textView = findView("跳过视频")) {
         clickButton(textView);
     }
@@ -224,9 +239,10 @@ function watchAds() {
 /**
  * 在控制台输出某个视图及所有子视图
  * @param {UiObject} view 视图
- * @param {number} level 空格等级
+ * @param {number|undefined} level 空格等级
  */
 function logView(view, level) {
+    if (!level) level = 0;
     let s = "";
     for (let i = 0; i < level; ++i) s += " ";
     log(`${s}${view}`);
@@ -250,7 +266,7 @@ function logRootView(child) {
         pv = child.parent();
     }
     log(pl);
-    logView(child, 0);
+    logView(child);
 }
 
 // #endregion
